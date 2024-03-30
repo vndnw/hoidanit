76dc683bf5ea1145e0ed0a603071afd261cb69f1
Duy Vo <vndnetwork@hotmail.com>
2024-03-31 00:06:23 +0700

    Add to do functionality and delete functionality to the To Do App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,62 @@
+import { useState } from "react";
+
 function App() {
+  const [todos, setTodos] = useState(["eat", "code", "sleep"]); // [1, 2, 3
+  const [value, setValue] = useState("");
+  const haha = Math.floor(new Date().valueOf() + Math.random());
+  console.log(`${haha}`);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) {
+      alert("Please enter a value");
+      return;
+    }
+    setTodos([...todos, value]);
+    setValue("");
+    alert("Form submitted");
+  };
+  const handleDelete = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
   return (
     <>
-      <h1 className="bg-red-700 text-3xl font-bold underline">Hello world!</h1>
+      <h1 className=" text-3xl font-bold underline">To Do App</h1>
+      <form className="flex flex-col" onSubmit={handleSubmit}>
+        <label htmlFor="id">Add to do:</label>
+        <input
+          value={value}
+          onChange={(e) => {
+            if (!e.target.value.startsWith(" ")) {
+              setValue(e.target.value);
+            }
+          }}
+          id="user"
+          type="text"
+          name=""
+          placeholder="Enter to do"
+        />
+        <button
+          onClick={handleSubmit}
+          className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+        >
+          Add
+        </button>
+      </form>
+      <div>
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index} className="flex justify-between">
+              <span>{todo}</span>
+              <button
+                onClick={() => handleDelete(index)}
+                className="rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
